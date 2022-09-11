2d43846ef72964a4de397ae0b5c0cee48ece36f4
= <v254373@dac.unicamp.br>
2022-09-11 16:56:37 -0300

    Realocando métodos em novas classes mais adequadas

---
+++ src/main/java/Controller/SaveFiles.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Victor Hugo Costa Osses, 254373
+ * @autor Pedro Trama Fernandes Pereira, 254344
+ * @authos Luis Francisco Ribeiro Malaquias, 254289
+ * @autor Jorge Yudi Saito Maruyama, 254274
+ */
+public class SaveFiles {
+    
+}
