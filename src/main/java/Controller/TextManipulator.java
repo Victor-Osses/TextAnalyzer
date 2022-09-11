cc3e2a2b585f7128724e6b954b94e832958902f0
= <v254373@dac.unicamp.br>
2022-09-11 00:11:36 -0300

    Adicionando classe com métodos para manipular textos

--- src/main/java/Controller/TextManipulator.java
+++ src/main/java/Controller/TextManipulator.java
@@ -3,10 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeMap;
 
 /**
  *
@@ -15,71 +14,36 @@ import java.util.Scanner;
  * @authos Luis Francisco Ribeiro Malaquias, 254289
  * @autor Jorge Yudi Saito Maruyama, 254274
  */
+
 public class TextManipulator {
-    private List fileNames;
-    
-    public TextManipulator(List fileNames) {
-        setFileNames(fileNames);
-    }
-    
-    public void getFiles() {
-       try {
-         for(int index = 0; index < this.getFileNames().size(); index++) {
-
-                 File file = new File(this.getFileNames().get(index).toString());
-                 Scanner scan = new Scanner(file);
-                 String text = "";
-
-                 while (scan.hasNextLine()) {
-                    text += scan.nextLine() + '\n';
-                 }
-                 
-                 System.out.println(text);
-                 scan.close();  
-         }
-       } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
-       }
-    }
-    
-    public void textAnalyzer(String text) {
+  
+    public ArrayList<TreeMap<String, ArrayList<String>>> textAnalyzer(ArrayList<ArrayList<String>> filesTextLines) {
+        ArrayList<TreeMap<String, ArrayList<String>>> wordCorrelation = new  ArrayList<>();
+       
+         for(ArrayList<String> text : filesTextLines) {
+       
+            for(String line : text) {
+                String[] words = line.trim().split(" ");
+                TreeMap<String, ArrayList<String>> tree = new TreeMap<>();
+                
+                for(int index = 0; index < words.length - 1; index++) {
+                   if(!tree.containsKey(words[index])) {
+                        tree.put(words[index], new ArrayList<>());
+                    }
+                   
+                    tree.get(words[index]).add(words[index + 1]);
+                }
+               wordCorrelation.add(tree);
+            }
+        }
         
-        textRegex(text); //Luis
-        //criar estrutra treemap //Osses
-        //fazer função que ordena os indíces da Treemap //Trama e Jorge 
-        //Criar arquivo e baixar ele na máquina do usuário //Tosi
-        
-        //OBS: Todos os blocos devem estar dentro de blocos Try Catch e suas excessões devem ser tratadas de forma especializada
-        //OBS: Tudo deve estar em inglês
-    /*  
-        - Ler cada linha do texto
-        - Implementar uma função para retirar a pontuação, aspas, travessões  etc - regex é uma solução
-        - Retornar a linha formatada corretamente
-        - Separar palavras da linha por espaço (guardar em um array list cada palavra)
-        - Associar cada palavra com a palavra posterior (ver estrutura tree map)
-            Exemplo de treemap (acho que é algo assim):
-               feliz dia aniversario
-               feliz ano
- 
-               ['feliz'] => ['dia', 'aniversario', 'ano']
-               ['pedro'] => ['dia', 'aniversario', 'ano']
-       - Depois de associar todas as palavras, ordernar por indíice do array 
-       - Passar treemap para o controller, do controller para saveFile que vai escrever a estrutura dentro de um arquivo e baixar 
-    */
+        return wordCorrelation;
     }
+   
     
     /*Tirar acentos, pontuação, travessão etc*/
     public String textRegex(String text) {
-        
         return "texto formatado";
     }
 
-    public List getFileNames() {
-        return fileNames;
-    }
-
-    public void setFileNames(List fileNames) {
-        this.fileNames = fileNames;
-    }
 }
