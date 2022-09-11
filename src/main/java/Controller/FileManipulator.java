0ceda112dbf907cf82608214f9f000dfd97f2869
= <v254373@dac.unicamp.br>
2022-09-11 16:53:32 -0300

    Adicionando métodos para criar arquivos e escrever textos neles

--- src/main/java/Controller/FileManipulator.java
+++ src/main/java/Controller/FileManipulator.java
@@ -6,9 +6,13 @@ package Controller;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * @author Victor Hugo Costa Osses, 254373
@@ -20,11 +24,11 @@ public class FileManipulator {
     
     public FileManipulator() {}
     
-     public List<String> scanFileNames() {
-        List<String> fileNames = new ArrayList();
-        System.out.println("Informe o caminho dos arquivos (informe vazio para parar)");
-        
+     public List<String> scanFileNames() { 
         try {
+            List<String> fileNames = new ArrayList();
+            System.out.println("Informe o caminho dos arquivos (informe vazio para parar)");
+            
             while(true) {
                 Scanner scan = new Scanner(System.in);
                 String str = scan.nextLine();
@@ -32,51 +36,93 @@ public class FileManipulator {
                 if(!str.equals("")) fileNames.add(str); 
                 else  break;
             }
+            
+          return fileNames;
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
-        return fileNames;
+        return null;
     }
 
      public ArrayList<File> getFiles(List fileNames) {
-        ArrayList<File> files = new ArrayList<File>();
          try {
+            ArrayList<File> files = new ArrayList<File>();
+            
             for(int index = 0; index < fileNames.size(); index++) {
                 File file = new File(fileNames.get(index).toString());
                 files.add(file);
            }
+            
+           return files;
          } catch (Exception e) {
               System.out.println(e.getMessage());
-         }
-         return files;
+         } 
+         return null;
      }
      
-      public ArrayList<ArrayList<String>> getFilesTextLines(ArrayList<File> files) {
-          ArrayList<ArrayList<String>> fileTexts = new ArrayList<ArrayList<String>>();
+      public  ArrayList<String>  getFilesText (ArrayList<File> files) {
           try {
+             ArrayList<String> fileTexts = new  ArrayList<String>();
+             
              for(int index = 0; index < files.size(); index++) {
-                ArrayList<String> fileLines = new ArrayList<String>();
+                String fileLines = "";
+                
                 Scanner scan = new Scanner(files.get(index));
                 
                 while (scan.hasNextLine()) {
-                    String line = scan.nextLine();
-                    if(!line.equals(""))
-                        fileLines.add(line);
+                    String line = scan.nextLine().replace("\n", "") + " ";
+                    fileLines += line;
                 }
-                scan.close();  
+                
                 fileTexts.add(fileLines);
+                scan.close();  
              }
+             
+            return fileTexts;
           }
           catch (FileNotFoundException e) {
               System.out.println("File not found");
               e.printStackTrace();
-         }
+          } 
           
-          return fileTexts;
+          return null;
       }
       
-      public void saveNewFiles(ArrayList<ArrayList<String>> newFiles) {
-          
+  
+      public void writeToFile(TreeMap<String, ArrayList<String>> text, File file) {
+            try {
+                FileWriter myWriter = new FileWriter(file.getName());
+                for(Map.Entry<String, ArrayList<String>> entry : text.entrySet()) {
+                    String line = "";
+                    for(String word : entry.getValue()) {
+                        line += word + " ";
+                    }
+                    
+                   myWriter.write(entry.getKey() + " " + line + "\n");
+                }
+                myWriter.close();
+              } 
+              catch (IOException e) {
+                  System.out.println("Error");
+                     e.printStackTrace();
+              }
+      }
+      
+      
+      public File createFiles(int index) {
+            try {
+                 File file = new File("Texto0" + index + ".txt");
+                 
+                 if (!file.createNewFile()) {
+                     System.out.println("File not created");
+                 } 
+                 
+                 return file; 
+             } catch (IOException e) {
+                System.out.println("Error");
+                e.printStackTrace();
+             } 
+          return null;
       }
 }
